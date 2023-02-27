4a166c22e53cd5f0658be638b7916a6bde5447a8
Leandro <leandromllr9@gmail.com>
2023-02-27 11:22:12 -0300

    feat: criando rota para acessar todos os filmes cadastrados

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,14 +1,17 @@
 import Fastify from "fastify";
+import { movieRoutes } from "./modules/movies/movie.route";
+import { moviesSchema } from "./modules/movies/movie.schemas";
 
 const server = Fastify()
 
-server.get('/', ()=>{
-    return {
-        message: 'Deu bom'
-    }
-})
 
 const main = async()=>{
+    server.register(movieRoutes, {prefix: "/api/movies"})
+
+    for( const schema of moviesSchema){
+        server.addSchema(schema)
+    }
+
     try{
         await server.listen({port: 3000})
         console.log('Server online na porta 3000')
